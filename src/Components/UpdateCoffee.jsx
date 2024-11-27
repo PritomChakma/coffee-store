6461ce7825806604f1ee0f9109473b9d423bbd7a
pritom <chakmapritom1@gmail.com>
2024-11-27 13:43:41 +0600

    update the coffee

--- src/Components/UpdateCoffee.jsx
+++ src/Components/UpdateCoffee.jsx
@@ -14,20 +14,41 @@ const UpdateCoffee = () => {
     const category = form.category.value;
     const Price = form.Price.value;
     const photo = form.photo.value;
-    const newCoffee = {
+    const updatedCoffee = {
       name,
       quantity,
       category,
       Price,
       photo,
     };
-    console.log(newCoffee);
+    console.log(updatedCoffee);
+       // send data to the server
+       fetch(`http://localhost:5000/coffees/${_id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(updatedCoffee),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.modifiedCount>0) {
+            Swal.fire({
+              title: "success!",
+              text: "Coffee added successfully",
+              icon: "success",
+              confirmButtonText: "Cool",
+            });
+          }
+        });
+      // form.reset();
   };
 
   return (
     <div className="bg-[#F4F3F0] md:p-24 ">
       <h2 className="text-3xl font-bold text-center my-5">
-      Update Existing Coffee Details
+        Update Existing Coffee Details
       </h2>
       <p className="text-center md:w-10/12 mx-auto mb-10">
         It is a long established fact that a reader will be distraceted by the
@@ -46,6 +67,7 @@ const UpdateCoffee = () => {
             <input
               type="text"
               name="name"
+              defaultValue={name}
               placeholder="Coffee Name"
               className="input input-bordered w-full"
               required
@@ -60,6 +82,7 @@ const UpdateCoffee = () => {
             <input
               type="text"
               name="quantity"
+              defaultValue={quantity}
               placeholder="Available Quantity"
               className="input input-bordered w-full"
               required
@@ -75,6 +98,7 @@ const UpdateCoffee = () => {
             <input
               type="text"
               name="category"
+              defaultValue={category}
               placeholder="category"
               className="input input-bordered w-full"
               required
@@ -87,6 +111,7 @@ const UpdateCoffee = () => {
             <input
               type="number"
               name="Price"
+              defaultValue={Price}
               placeholder="Price"
               className="input input-bordered w-full"
               required
@@ -101,6 +126,7 @@ const UpdateCoffee = () => {
           <input
             type="text"
             name="photo"
+            defaultValue={photo}
             placeholder="Enter photo url"
             className="input input-bordered w-full"
             required
@@ -108,7 +134,7 @@ const UpdateCoffee = () => {
         </div>
         <input
           type="submit"
-          value="Add coffee "
+          value="Update coffee "
           className="btn btn-block bg-[#D2B48C] my-7 px-2"
         />
       </form>
